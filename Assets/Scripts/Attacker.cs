63609516db06ab4c517a35636a5ec224d62131c8
Pedro Bacchini <pedrobacchini@outlook.com>
2020-02-24 22:34:56 -0300

    Spawn Attackers with Unirx

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)] 
+    [SerializeField] private float walkSpeed = 1;
+
+    private void Start()
+    {
+        this.UpdateAsObservable()
+            .Subscribe(_ => transform.Translate(walkSpeed * Time.deltaTime * Vector2.left))
+            .AddTo(this);
+    }
+}
---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] private Attacker lizardPrefab = null;
+    [SerializeField] private float minSpawnDelay = 1;
+    [SerializeField] private float maxSpawnDelay = 5;
+    private readonly CompositeDisposable _disposables = new CompositeDisposable();
+
+    private void Start()
+    {
+        Observable.Return(Unit.Default)
+            .SelectMany(_ => Observable.Timer(TimeSpan.FromSeconds(Random.Range(minSpawnDelay, maxSpawnDelay + 1))))
+            .Do(_ => SpawnAttacker())
+            .Repeat()
+            .Subscribe()
+            .AddTo(_disposables);
+
+        this.OnDisableAsObservable()
+            .Do(_ => _disposables.Clear())
+            .Subscribe();
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(lizardPrefab, transform.position, Quaternion.identity);
+    }
+}
