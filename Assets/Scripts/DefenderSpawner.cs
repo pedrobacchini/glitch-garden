17e74e3ed71bb124b351c7917a38485fc49aa0ba
Pedro Bacchini <pedrobacchini@outlook.com>
2020-02-25 16:37:22 -0300

    Spawn Defender

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject defender = null;
+
+    private void Start()
+    {
+        this.OnMouseDownAsObservable()
+            .Subscribe(_ => Instantiate(defender, transform.position, Quaternion.identity));
+    }
+}
